e771245985928865472c0a1cf71d008e9e81f4fd
guidrymwg <guidry@utk.edu>
2016-08-31 20:11:59 -0400

    Added floating action button to the answer screen that opens a snackbar with the option of choosing a new quiz subject. All seems to work on Nexus 6P.

--- app/src/main/java/com/lightcone/quizzer/AnswerScreen.java
+++ app/src/main/java/com/lightcone/quizzer/AnswerScreen.java
@@ -2,9 +2,13 @@ package com.lightcone.quizzer;
 
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -24,6 +28,7 @@ public class AnswerScreen extends AppCompatActivity {
     private static int numberRight;
     private int numberWrong;
     private int score;
+    Context context;
 
     public int bkg;  // Holds int identifying background image
 
@@ -31,6 +36,8 @@ public class AnswerScreen extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.answerscreen);
+
+        context = getBaseContext();
 
         // Choose the background image
         switch(AstroQA.subjectIndex){
@@ -53,6 +60,29 @@ public class AnswerScreen extends AppCompatActivity {
         } else {
             layout.setBackground( getResources().getDrawable(bkg) );
         }
+
+        // Create a floating action button
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fabnew);
+
+        // Add a click listener to the floating action button that launches a snackbar
+        // and sets a listener on it to deal with clicks on the action button of the
+        // snackbar.
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, R.string.snackText, Snackbar.LENGTH_INDEFINITE)
+                        .setAction(R.string.snackButtonText, new View.OnClickListener() {
+                            // Handle clicks on snackbar button
+                            @Override
+                            public void onClick(View v) {
+                                Log.i("SNACK", "Snackbar button was clicked");
+                                Intent i = new Intent(context, MainActivity.class);
+                                startActivity(i);
+                            }
+                        }).show();
+            }
+        });
 
         numberRight = AstroQA.numberRight;
         numberWrong= AstroQA.numberWrong;
