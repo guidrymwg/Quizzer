08ac5714aed7b9338af5b8c66fdf7961dc71758c
guidrymwg <guidry@utk.edu>
2016-09-10 17:00:09 -0400

    refactorin names to more descriptive values for nav drawer.

--- app/src/main/java/com/lightcone/quizzer/MainActivity.java
+++ app/src/main/java/com/lightcone/quizzer/MainActivity.java
@@ -21,8 +21,9 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    long quizSelected = -1;
+    //long quizSelected = -1;
     Context context;
+    static final String TAG="QUIZZER";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,9 +37,7 @@ public class MainActivity extends AppCompatActivity
         // Create a floating action button
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 
-        // Add a click listener to the floating action button that launches a snackbar
-        // and sets a listener on it to deal with clicks on the action button of the
-        // snackbar.
+        // Add a click listener to the floating action button
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -92,55 +91,41 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
+
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.astro_quiz) {
+        if (id == R.id.astronomy) {
 
-           /* AstroQA.subjectIndex = 0;
-            Intent i = new Intent(this, AstroQA.class);
-            startActivity(i);*/
+            Log.i(TAG,"Astronomy nav drawer");
 
-/*            // Handle the camera action
-            Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-            startActivity(intent);*/
+        }
 
-        } else if (id == R.id.nav_call) {
+        else if (id == R.id.history) {
 
-            /*AstroQA.subjectIndex = 1;
-            Intent i = new Intent(this, AstroQA.class);
-            startActivity(i);*/
+            Log.i(TAG,"History nav drawer");
 
-            /*String number = "202-456-1111";   // White House phone number
-            String numberToDial = "tel:" + number;
-            startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(numberToDial)));*/
+        }
 
-        } else if (id == R.id.nav_slideshow) {
+        else if (id == R.id.mathematics) {
 
-            // Will open to your google slides account if you have one, assuming
-            // that you are logged into your Google account on the device.
+            Log.i(TAG,"Math nav drawer");
 
-            Intent markerIntent = new Intent(Intent.ACTION_VIEW);
-            markerIntent.setData(Uri.parse("https://docs.google.com/presentation/u/0/"));
-            startActivity(markerIntent);
+        }
 
-            // This will only work if dropbox app is installed
-            /*Intent intent = getPackageManager().getLaunchIntentForPackage(
-                    "com.dropbox.android");
-            startActivity(intent);*/
+        else if (id == R.id.geography) {
 
-            /*Intent markerIntent=new Intent(Intent.ACTION_VIEW);
-            markerIntent.setData(Uri.parse("https://www.dropbox.com/login"));
-            startActivity(markerIntent);*/
+            Log.i(TAG,"Geography nav drawer");
 
-        } else if (id == R.id.nav_drive) {
+        }
 
-            // Will open to your google drive account if you have one
-            Intent markerIntent = new Intent(Intent.ACTION_VIEW);
-            markerIntent.setData(Uri.parse("https://drive.google.com/drive"));
-            startActivity(markerIntent);
+        else if (id == R.id.biology) {
 
-        } else if (id == R.id.nav_share) {
+            Log.i(TAG,"Biology nav drawer");
+
+        }
+
+        else if (id == R.id.nav_share) {
 
             // Will open to my Google+ homepage.  Change gid to your G+ id
             // to access your G+ page.  To get your id, open Google+ with a
@@ -151,7 +136,9 @@ public class MainActivity extends AppCompatActivity
             String add = "https://plus.google.com/" + gid + "/posts";
             startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(add)));
 
-        } else if (id == R.id.nav_play) {
+        }
+
+        else if (id == R.id.nav_play) {
 
             // Open the Play Store
 
@@ -160,14 +147,6 @@ public class MainActivity extends AppCompatActivity
             startActivity(markerIntent);
         }
 
-        else if (id == R.id.nav_biology) {
-
-            // Open the Play Store
-
-            Intent markerIntent = new Intent(Intent.ACTION_VIEW);
-            markerIntent.setData(Uri.parse("https://play.google.com/store"));
-            startActivity(markerIntent);
-        }
 
         // Close the navigation drawer and return true indicating that the event
         // has been handled.
